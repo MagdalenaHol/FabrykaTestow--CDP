cef0a2e3cc248c53efa2252570281b2fb3b92378
Magda Holda <magdalena.holda2@gmail.com>
2025-06-04 00:09:46 +0200

    Test cookies method

--- cypress/e2e/MOD5/TS1.cy.js
+++ cypress/e2e/MOD5/TS1.cy.js
@@ -1,4 +1,18 @@
-decribe('', () => {
+describe('my first scenario', () => {
+    it('visit main page and click top menu button', function() {
+        cy.visit('https://fabrykatestow.pl')
+
+        // cy.get('#nv-primary-navigation-main').contains('Newsletter').click()
+
+        cy.getAllCookies()
+        cy.getCookies()
+        cy.getCookie('_gid')
+        const a = cy.getAllLocalStorage()
+
+        console.log(a)
+
+       
+    })
 
 
 })

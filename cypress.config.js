abb0f390577bb43891ec533d2f3baf07fb425a35
Magda Holda <magdalena.holda2@gmail.com>
2025-06-04 00:07:12 +0200

    Fix height and width of test website

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 700,
+  viewportWidth: 1080,
+
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
